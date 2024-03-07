68ffddfa936619433563f2708010c6b8428866ce
Paolo Preziuso <preziusopao@hotmail.it>
2024-03-07 19:34:26 +0100

    Enhance asset upload util

--- asset_manager/asset_manager.py
+++ asset_manager/asset_manager.py
@@ -1,17 +1,34 @@
+import hou
 import os
 import json
+import shutil
 from DropboxUtil import DropboxUtils
 
-def upload_asset(asset_path, asset_name, ext="bgeo.sc"):
+def upload_asset(node):
+  # find library path destination
+  asset_path = node.parm('file').eval()
+  asset_name = asset_path.split('/')[-1]
+  ext = ''
+  if 'bgeo.sc' in asset_name:
+    asset_name = asset_name.replace('.bgeo.sc', '')
+    ext = 'bgeo.sc'
+  if 'vdb' in asset_name:
+    asset_name = asset_name.replace('.vdb', '')
+    ext = 'vdb'
   destpath = '/' + asset_name + '/' + asset_name + '.' + ext
+  
+  # copy into local library
+  shutil.copyfile(asset_path, '_assets' + destpath)
 
+  # upload into cloud library
   man = DropboxUtils()
   asset = man.uploadAsset(destpath, overwrite=True)
   asset_link = man.getSharedLink(destpath)
 
-  updateLibrary(asset, asset_link)
+  # update asset info
+  update_library(asset, asset_link)
 
-def updateLibrary(asset, asset_link):
+def update_library(asset, asset_link):
   if not os.path.exists('assetlib.json') or os.path.getsize('assetlib.json') == 0:
     with open('assetlib.json', 'w') as lib_file:
       json.dump({}, lib_file, indent=2)
