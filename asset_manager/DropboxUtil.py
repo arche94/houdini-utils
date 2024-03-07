9af2d70be009d2e6d6529a18c1955ed785e540db
Paolo Preziuso <preziusopao@hotmail.it>
2024-03-07 17:34:27 +0100

    Implement up-download from Dropbox

--- asset_manager/DropboxUtil.py
+++ asset_manager/DropboxUtil.py
@@ -1,8 +1,11 @@
 import os
 import dropbox
 import yaml
+import json
 import requests
 import webbrowser
+import datetime
+import time
 
 class DropboxUtils:
   def __init__(self):
@@ -12,6 +15,8 @@ class DropboxUtils:
       except yaml.YAMLError as e:
         print(e)
     
+    self.__local_basepath = config['assets']['basepath']
+
     self.__app_key = config['dropbox']['app_key']
     self.__app_secret = config['dropbox']['app_secret']
     self.__refresh_token = config['dropbox']['refresh_token'] if 'refresh_token' in config['dropbox'] else None
@@ -21,12 +26,75 @@ class DropboxUtils:
       self.__getRefreshToken()
     self.__getSession()
 
-  def getFileList(self, location=''):
-    return self.__session.files_list_folder(location)
+  def getFileList(self, location='', list_subfolders=False):
+    entries = self.__session.files_list_folder(location, recursive=list_subfolders).entries
+    entries.sort(key=lambda e: e.path_display)
+    return entries
   
   def getUsername(self):
     user = self.__session.users_get_current_account()
     return user.name.display_name
+
+  def uploadAsset(self, filepath, overwrite=False):
+    fullpath = os.path.abspath(os.path.join(self.__local_basepath, filepath)).replace("\\", "/")
+    mode = dropbox.files.WriteMode.overwrite if overwrite else dropbox.files.WriteMode.add
+    mtime = os.path.getmtime(fullpath)
+
+    if not filepath.startswith('/'):
+      filepath = '/' + filepath
+
+    with open(fullpath, 'rb') as asset_file:
+      asset_data = asset_file.read()
+
+    try:
+      res = self.__session.files_upload(
+        asset_data, 
+        filepath, 
+        mode=mode, 
+        client_modified=datetime.datetime(*time.gmtime(mtime)[:6])
+      )      
+    except dropbox.exceptions.ApiError as e:
+      print(f"Error uploading {filepath} to Dropbox: {e}")
+      return
+    
+    print(f"Successfully uploaded {res.name.encode('utf8')}")
+    return res
+
+  def downloadAsset(self, entry, overwrite=False):
+    path = entry.path_display
+    fullpath = os.path.abspath(os.path.join(self.__local_basepath, path[1:])).replace('\\', '/')
+
+    if os.path.exists(fullpath) and not overwrite:
+      print(f"The requested files already exists: {path}")
+      return
+    
+    if len(path[1:].split('/')) > 1:
+      try:     
+        os.makedirs('/'.join(fullpath.split('/')[:-1]), exist_ok=True)
+      except OSError as oe:
+        print(f"Error creating folder {fullpath}: {oe}")
+        return
+    
+    try:
+      res = self.__session.files_download_to_file(fullpath, path)
+    except dropbox.exceptions.ApiError as e:
+      print(f"Error downloading asset {path} from Dropbox: {e}")
+      return
+    
+    print(f"File successfully downloaded at {fullpath}")
+    return res
+
+  def getSharedLink(self, asset_path):
+    if not asset_path.startswith('/'):
+      asset_path = '/' + asset_path
+
+    try:
+      link = self.__session.sharing_create_shared_link(asset_path)
+    except dropbox.exceptions.ApiError as e:
+      print(f"Error getting shared link for {asset_path}: {e}")
+      return None
+    
+    return link
 
   def __getRefreshToken(self):
     webbrowser.open(f"https://www.dropbox.com/oauth2/authorize?client_id={self.__app_key}&token_access_type=offline&response_type=code")
@@ -61,8 +129,49 @@ class DropboxUtils:
       self.__getSession()
 
 if __name__ == "__main__":
+  test_assets = [
+    'test.txt',
+    'subdir/test_nested.txt'
+  ]
+
   dbx = DropboxUtils()
   print(dbx.getUsername())
-  for e in dbx.getFileList().entries:
-    print(e.name)
+
+  if not os.path.exists('testlib.json') or os.path.getsize('testlib.json') == 0:
+    with open('testlib.json', 'w') as lib_file:
+      json.dump({}, lib_file, indent=2)
+
+  with open('testlib.json', 'r') as lib_file:
+    test_lib = json.load(lib_file)
+
+  for asset in test_assets:
+    dbx_asset = dbx.uploadAsset(asset)
+    dbx_shrlnk = dbx.getSharedLink(asset)
+    entry = {
+      "name": dbx_asset.name,
+      "path": f"{dbx_asset.path_display}",
+      "shared_link": f"{dbx_shrlnk.url}"
+    }
+    test_lib[dbx_asset.id] = entry
+
+  with open('testlib.json', 'w') as lib_file:
+    json.dump(test_lib, lib_file, indent=2)
+
+  for e in dbx.getFileList(list_subfolders=True):
+    if not isinstance(e, dropbox.files.FolderMetadata):
+      if e.id in test_lib:
+        print(f"File already exists: {e.path_display}")
+      else:
+        print(f"Downloading: {e.path_display} ...")
+        dbx_asset = dbx.downloadAsset(e)
+        dbx_shrlnk = dbx.getSharedLink(e.path_display)
+        entry = {
+          "name": dbx_asset.name,
+          "path": f"{dbx_asset.path_display}",
+          "shared_link": f"{dbx_shrlnk.url}"
+        }
+        test_lib[dbx_asset.id] = entry
+
+  with open('testlib.json', 'w') as lib_file:
+    json.dump(test_lib, lib_file, indent=2)
   
